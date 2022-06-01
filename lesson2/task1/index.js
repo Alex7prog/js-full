06c5b92dc2a7bb809027dbcb07b77c85f84f5aa4
Alex Sedmoy <alexsedmoy@gmail.com>
2022-06-01 12:41:26 +0300

    first commit

---
+++ lesson2/task1/index.js
@@ -0,0 +1,13 @@
+/*
+const User Name = 'John';
+const 1greeting = "I'm John";
+const ho%b@by = 'Walking';
+const FavouriteDESSERT = 'cake';
+const super&Number = 17;
+*/
+
+const userName = 'John';
+const greeting = "I'm John";
+const hobby = 'Walking';
+const favouriteDessert = 'cake';
+const superNumber = 17;
