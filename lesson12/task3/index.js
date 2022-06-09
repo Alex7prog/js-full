b891daa0062bd181e2fbe5b3a0ba71d2e6c78934
Alex Sedmoy <alexsedmoy@gmail.com>
2022-06-09 18:53:55 +0300

    clearing transactions

---
+++ lesson12/task3/index.js
@@ -0,0 +1,9 @@
+// clearing transactions data
+
+const cleanTransactionsList = (transactionArr) => {
+  return transactionArr
+    .filter((elem) => Number(elem))
+    .map((elem) => `$${Number(elem).toFixed(2)}`);
+};
+
+console.log(cleanTransactionsList([' 1.9', '2', 3, '4 usd', '5.14 ', ' 6 ']));
