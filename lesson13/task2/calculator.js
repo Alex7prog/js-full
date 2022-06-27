664ed8ef08b5929f4240d002e92f8efa33e05f78
Alex Sedmoy <alexsedmoy@gmail.com>
2022-06-27 15:15:40 +0300

    Test code in different files

--- lesson13/task2/calculator.js
+++ lesson13/task2/calculator.js
@@ -5,6 +5,5 @@ export const getSquaredNumbers = arr => arr.map(num => num ** 2);
 export const getOddNumbers = arr => arr.filter(num => num % 2 === 1);
 
 // const sum = (a, b) => a + b;
-
-// default export
+// function sum default export
 export default (a, b) => a + b;
