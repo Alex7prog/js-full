81b44b3e56c6feb54f29a2704cbdfc8d7c72b8b2
Alex Sedmoy <alexsedmoy@gmail.com>
2022-06-28 23:43:39 +0300

    logger

---
+++ lesson15/task3/index.js
@@ -0,0 +1,60 @@
+// logger
+
+'use strict';
+
+/**
+ * @return {object}
+ */
+
+export const createLogger = () => {
+  const logMemory = [];
+
+  const warn = msg => {
+    logMemory.unshift({ message: msg, type: 'warn', dateTime: new Date() });
+  };
+
+  const error = msg => {
+    logMemory.unshift({ message: msg, type: 'error', dateTime: new Date() });
+  };
+
+  const log = msg => {
+    logMemory.unshift({ message: msg, type: 'log', dateTime: new Date() });
+  };
+
+  const getRecords = typeMsg => {
+    if (!typeMsg) {
+      return logMemory;
+    }
+
+    return logMemory.filter(obj => obj.type === typeMsg);
+  };
+
+  return {
+    warn,
+    error,
+    log,
+    getRecords,
+  };
+};
+
+// examples
+const logger1 = createLogger();
+logger1.log('User logged in');
+logger1.warn('User is tring to ented restricted page');
+logger1.log('User logged out');
+logger1.error('Unexpected error on the site');
+
+logger1.getRecords(); // ===> [{ message: 'Unexpected error on the site', type: 'error', dateTime: Date }, { message: 'User logged out', type: 'log', dateTime: Date }, { message: 'User is tring to ented restricted page', type: 'warn', dateTime: Date }, { message: 'User logged in', type: 'log', dateTime: Date }]
+logger1.getRecords('log'); // ===> [{ message: 'User logged out', type: 'log', dateTime: Date }, { message: 'User logged in', type: 'log', dateTime: Date }]
+logger1.getRecords('error'); // ===> [{ message: 'Unexpected error on the site', type: 'error', dateTime: Date }]
+logger1.getRecords('warn'); // ===> [{ message: 'User is tring to ented restricted page', type: 'warn', dateTime: Date }]
+
+const logger2 = createLogger();
+logger2.warn('Opps, something is happenning');
+logger2.getRecords('error'); // ===> []
+logger2.getRecords('warn'); // ===> [{ message: 'Opps, something is happenning', type: 'warn', dateTime: Date }]
+logger2.getRecords(); // ===> [{ message: 'Opps, something is happenning', type: 'warn', dateTime: Date }]
+
+const logger3 = createLogger();
+logger3.getRecords('error'); // ===> []
+logger3.getRecords(); // ===> []
