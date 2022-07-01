062b7151620a1b9dd5bd20092163c0e329dfd80c
Alex Sedmoy <alexsedmoy@gmail.com>
2022-07-01 18:06:06 +0300

    Pseudo arrays, sumOfSquares()

--- lesson18/task3/index.js
+++ lesson18/task3/index.js
@@ -1,7 +1,11 @@
 // Pseudo arrays, sumOfSquares()
 
-export function sumOfSquares() {
-  return [...arguments].reduce((acc, arg) => acc + arg ** 2, 0);
+// export function sumOfSquares() {
+//   return [...arguments].reduce((acc, arg) => acc + arg ** 2, 0);
+// }
+
+export function sumOfSquares(...restArg) {
+  return [...restArg].reduce((acc, arg) => acc + arg ** 2, 0);
 }
 
 console.log(sumOfSquares(1, -2, '-3', '4'));
