c6f81d20b8a86966e82ab5516472e395425550ba
Alex Sedmoy <alexsedmoy@gmail.com>
2022-07-01 15:27:56 +0300

    vehicle and ship - prototype

---
+++ lesson19/task1/index.js
@@ -0,0 +1,33 @@
+// vehicle and ship - prototype
+
+export const vehicle = {
+  name: 'Argo',
+  move() {
+    console.log(`${this.name} is moving`);
+  },
+  stop() {
+    console.log(`${this.name} stopped`);
+  },
+};
+
+export const ship = {
+  name: 'sArgo',
+  startMachine() {
+    console.log(`${this.name} lifting anchor up`);
+    this.move();
+  },
+  stopMachine() {
+    this.stop();
+    console.log(`${this.name} lifting anchor down`);
+  },
+};
+
+Object.setPrototypeOf(ship, vehicle);
+
+console.log('ship: ', ship);
+
+ship.move();
+
+ship.startMachine();
+
+ship.stopMachine();
