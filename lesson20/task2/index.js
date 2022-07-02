4a03fe9292f78dad6682deaf438e96960ae4368d
Alex Sedmoy <alexsedmoy@gmail.com>
2022-07-02 18:16:09 +0300

    Classes Vehicle and Ship

---
+++ lesson20/task2/index.js
@@ -0,0 +1,40 @@
+// Classes Vehicle and Ship
+
+export class Vehicle {
+  constructor(name, numberOfWheels) {
+    this.name = name;
+    this.numberOfWheels = numberOfWheels;
+  }
+
+  move() {
+    console.log(`${this.name} is moving`);
+  }
+
+  stop() {
+    console.log(`${this.name} stopped`);
+  }
+}
+
+export class Ship extends Vehicle {
+  constructor(name, speed) {
+    super(name, false);
+    this.speed = speed;
+  }
+
+  move() {
+    console.log(`${this.name} lifting anchor up`);
+    super.move();
+  }
+
+  stop() {
+    super.stop();
+    console.log(`${this.name} lifting anchor down`);
+  }
+}
+
+const shipOne = new Ship('Argo', 19);
+
+console.log('shipOne: ', shipOne);
+
+shipOne.move();
+shipOne.stop();
