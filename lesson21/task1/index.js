d651d4bfd2ce43f6b66baa8b25227a6531246a61
Alex Sedmoy <alexsedmoy@gmail.com>
2022-07-04 09:53:35 +0300

    Todo List - render list

---
+++ lesson21/task1/index.js
@@ -0,0 +1,43 @@
+// Todo List - render list
+
+export const tasks = [
+  { text: 'Buy milk', done: false },
+  { text: 'Pick up Tom from airport', done: false },
+  { text: 'Visit party', done: false },
+  { text: 'Visit doctor', done: true },
+  { text: 'Buy meat', done: true },
+];
+
+/**
+ * @param {object[]} tasksList
+ * @return {undefined}
+ */
+
+const renderTasks = tasksList => {
+  const listElem = document.querySelector('.list');
+
+  const listItemElems = tasksList
+    .sort((a, b) => a.done - b.done)
+    .map(({ text, done }) => {
+      const listItemElem = document.createElement('li');
+      listItemElem.classList.add('list__item');
+
+      const checkboxElem = document.createElement('input');
+      checkboxElem.setAttribute('type', 'checkbox');
+      checkboxElem.classList.add('list__item-checkbox');
+
+      if (done) {
+        checkboxElem.checked = done;
+        listItemElem.classList.add('list__item_done');
+      }
+
+      listItemElem.append(checkboxElem, text);
+
+      return listItemElem;
+    });
+
+  listElem.append(...listItemElems);
+  console.log('listElem: ', listElem);
+};
+
+renderTasks(tasks);
