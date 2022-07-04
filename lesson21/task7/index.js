b1d662328d3b826f12c42a643526f0d36bd31e42
Alex Sedmoy <alexsedmoy@gmail.com>
2022-07-04 16:39:55 +0300

    clear element

---
+++ lesson21/task7/index.js
@@ -0,0 +1,11 @@
+// clear element
+
+// clearList()
+// in: none
+// out: none, <.categories>.innerHTML = ''
+
+const clearList = function () {
+  document.querySelector('.categories').innerHTML = '';
+};
+
+clearList();
