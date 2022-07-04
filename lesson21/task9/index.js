2af058565cd0816c03bf25b4e086a0abeb44ecd6
Alex Sedmoy <alexsedmoy@gmail.com>
2022-07-04 22:22:17 +0300

    create DOM elements p2

--- lesson21/task9/index.js
+++ lesson21/task9/index.js
@@ -6,25 +6,32 @@
 
 export const finishList = function () {
   const elemList = document.querySelector('.list');
-  const elemListItems = elemList.querySelectorAll('li');
-  let elemListItem = document.createElement('li');
+  const elemListItemSpecial = document.querySelector('.special');
 
-  elemListItem.innerText = '1';
-  elemList.prepend(elemListItem);
+  elemList.prepend(document.createElement('li'));
+  elemList.firstElementChild.innerText = '1';
 
-  elemListItem = document.createElement('li');
-  elemListItem.innerText = '4';
-  elemListItems[2].before(elemListItem);
+  elemListItemSpecial.before(document.createElement('li'));
+  elemListItemSpecial.previousElementSibling.innerText = '4';
+  elemListItemSpecial.after(document.createElement('li'));
+  elemListItemSpecial.nextElementSibling.innerText = '6';
 
-  elemListItem = document.createElement('li');
-  elemListItem.innerText = '6';
-  elemListItems[2].after(elemListItem);
-
-  elemListItem = document.createElement('li');
-  elemListItem.innerText = '8';
-  elemList.append(elemListItem);
+  elemList.append(document.createElement('li'));
+  elemList.lastElementChild.innerText = '8';
 };
 
 finishList();
 
 //  console.log(document.querySelector('.list'));
+
+// elemList.prepend(document.createElement('li'));
+// elemList.children[0].innerText = '1';
+
+// elemList.children[3].before(document.createElement('li'));
+// elemList.children[3].innerText = '4';
+
+// elemList.children[4].after(document.createElement('li'));
+// elemList.children[5].innerText = '6';
+
+// elemList.append(document.createElement('li'));
+// elemList.children[7].innerText = '8';
