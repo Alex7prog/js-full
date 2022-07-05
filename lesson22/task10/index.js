08dd5427e5308cb69520ade8bacfca470ac4d751
Alex Sedmoy <alexsedmoy@gmail.com>
2022-07-05 14:59:28 +0300

    Handler for several elem

--- lesson22/task10/index.js
+++ lesson22/task10/index.js
@@ -1,11 +1,23 @@
 // Handler for several elem
 
-const elemButtonsPagination = document.querySelector('.pagination');
+const elemButtonsPagination = document.querySelectorAll('.pagination__page');
 
 const handlePagBtnClick = function (event) {
-  if (event.target.classList.contains('pagination__page')) {
-    console.log(event.target.dataset.pageNumber);
-  }
+  console.log(event.target.dataset.pageNumber);
 };
 
-elemButtonsPagination.addEventListener('click', handlePagBtnClick);
+elemButtonsPagination[0].addEventListener('click', handlePagBtnClick);
+elemButtonsPagination[1].addEventListener('click', handlePagBtnClick);
+elemButtonsPagination[2].addEventListener('click', handlePagBtnClick);
+
+// option 2 (best)
+
+// const elemButtonsPagination = document.querySelector('.pagination_page');
+
+// const handleClick = function (event) {
+//   if (event.target.classList.contains('pagination__page')) {
+//     console.log(event.target.dataset.pageNumber);
+//   }
+// };
+
+// elemButtonsPagination.addEventListener('click', handleClick);
