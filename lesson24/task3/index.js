fe71db51e764f483cc1189cd6cb38e4207a3d3f2
Alex Sedmoy <alexsedmoy@gmail.com>
2022-07-06 19:04:09 +0300

    Date difference

--- lesson24/task3/index.js
+++ lesson24/task3/index.js
@@ -3,10 +3,10 @@
 export const getDiff = function (startDate, endDate) {
   const differenceDateMS = Math.abs(startDate - endDate);
 
-  let seconds = parseInt((differenceDateMS / 1000) % 60);
-  let minutes = parseInt((differenceDateMS / (1000 * 60)) % 60);
-  let hours = parseInt((differenceDateMS / (1000 * 60 * 60)) % 24);
-  let days = parseInt(differenceDateMS / (1000 * 60 * 60 * 24));
+  const seconds = Math.trunc((differenceDateMS / 1000) % 60);
+  const minutes = Math.trunc((differenceDateMS / (1000 * 60)) % 60);
+  const hours = Math.trunc((differenceDateMS / (1000 * 60 * 60)) % 24);
+  const days = Math.trunc(differenceDateMS / (1000 * 60 * 60 * 24));
 
   return `${days}d ${hours}h ${minutes}m ${seconds}s`;
 };
