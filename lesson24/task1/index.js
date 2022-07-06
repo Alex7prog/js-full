8904994cd2afc0c23952fc0890445f7a83553bbe
Alex Sedmoy <alexsedmoy@gmail.com>
2022-07-06 16:52:35 +0300

    set  date component

--- lesson24/task1/index.js
+++ lesson24/task1/index.js
@@ -1,13 +1,13 @@
 // set  date component
 
+const weekDay = ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'St'];
+
 export const dayOfWeek = (date, days) => {
   const day = new Date(date).getDate();
   const dateInFuture = new Date(date).setDate(day + days);
 
   return weekDay[new Date(dateInFuture).getDay()];
 };
-
-const weekDay = ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'St'];
 
 const resultDate = dayOfWeek(new Date(2021, 11, 31), 7);
 
