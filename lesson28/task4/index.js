7c2f49ebbe71f5710332c6a9841b1c92464be045
Alex Sedmoy <alexsedmoy@gmail.com>
2022-07-08 13:59:55 +0300

    compose

---
+++ lesson28/task4/index.js
@@ -0,0 +1,18 @@
+// compose
+
+// compose():
+// in: (...funcsArg) / out: function/number
+
+export const compose =
+  (...funcs) =>
+  value =>
+    funcs.reduce((acc, func) => func(acc), value);
+
+const add2 = value => value + 2;
+const square = value => value * value;
+const half = value => value / 2;
+
+// test-data
+
+const superFunc = compose(add2, square, half);
+console.log(superFunc(4)); // 18
