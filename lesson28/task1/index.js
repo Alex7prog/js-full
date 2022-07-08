fc91f825296c72cdc12760f62d1c73b02360c240
Alex Sedmoy <alexsedmoy@gmail.com>
2022-07-08 11:50:03 +0300

    calc (chaining)

---
+++ lesson28/task1/index.js
@@ -0,0 +1,35 @@
+// calc (chaining)
+
+// i: number / u: {methods}
+
+const calc = value => {
+  let result = value;
+
+  const calculator = {
+    add(value) {
+      result += value;
+      return this;
+    },
+    subtract(value) {
+      result -= value;
+      return this;
+    },
+    mult(value) {
+      result *= value;
+      return this;
+    },
+    div(value) {
+      result /= value;
+      return this;
+    },
+    result(value) {
+      return result;
+    },
+  };
+
+  return calculator;
+};
+
+// test-data
+const result = calc(3).add(2).mult(4).div(10).subtract(5).result();
+console.log('result: ', result);
