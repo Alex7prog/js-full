1b4a9ed97726682249aa5f12099e9d322eff39d2
Alex Sedmoy <alexsedmoy@gmail.com>
2022-07-10 17:27:20 +0300

    Promisification

--- lesson30/task4/index.js
+++ lesson30/task4/index.js
@@ -13,16 +13,16 @@ export const addImageV2 = url => {
 };
 
 // examples
-const imgSrc =
-  'https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg';
+// const imgSrc =
+//   'https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg';
 
-const imgSrcBad =
-  'https://p.igstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg';
+// const imgSrcBad =
+//   'https://p.igstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg';
 
-addImageV2(imgSrc)
-  .then(data => console.log(data)) // ==> { width: 200, height: 100 }
-  .catch(error => console.log(error)); // ==> 'Image load failed'
+// addImageV2(imgSrc)
+//   .then(data => console.log(data)) // ==> { width: 200, height: 100 }
+//   .catch(error => console.log(error)); // ==> 'Image load failed'
 
-addImageV2(imgSrcBad)
-  .then(data => console.log(data)) // ==> { width: 200, height: 100 }
-  .catch(error => console.log(error)); // ==> 'Image load failed'
+// addImageV2(imgSrcBad)
+//   .then(data => console.log(data)) // ==> { width: 200, height: 100 }
+//   .catch(error => console.log(error)); // ==> 'Image load failed'
