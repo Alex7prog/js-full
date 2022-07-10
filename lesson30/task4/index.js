cc682f93a480bdcee202c95d0a8b3e21a9e3e83b
Alex Sedmoy <alexsedmoy@gmail.com>
2022-07-10 17:24:49 +0300

    Promisification

---
+++ lesson30/task4/index.js
@@ -0,0 +1,28 @@
+// Promisification
+
+import { addImage } from './solution/addImage.js';
+
+export const addImageV2 = url => {
+  const promise = new Promise((resolveImgLoad, rejectImgLoad) => {
+    addImage(url, (error, ...rest) => {
+      error ? rejectImgLoad(new Error(error)) : resolveImgLoad(...rest);
+    });
+  });
+
+  return promise;
+};
+
+// examples
+const imgSrc =
+  'https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg';
+
+const imgSrcBad =
+  'https://p.igstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg';
+
+addImageV2(imgSrc)
+  .then(data => console.log(data)) // ==> { width: 200, height: 100 }
+  .catch(error => console.log(error)); // ==> 'Image load failed'
+
+addImageV2(imgSrcBad)
+  .then(data => console.log(data)) // ==> { width: 200, height: 100 }
+  .catch(error => console.log(error)); // ==> 'Image load failed'
