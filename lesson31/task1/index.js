2f12445b019d88485462b38cc770a3e2a4b6f1c9
Alex Sedmoy <alexsedmoy@gmail.com>
2022-07-10 23:22:34 +0300

    Promise handlers

--- lesson31/task1/index.js
+++ lesson31/task1/index.js
@@ -9,28 +9,29 @@
 export const requestUserData = userId => {
   const prRequestUserData = new Promise((rulfilled, rejected) => {
     if (userId === 'broken') {
-      rejected(new Error('User not found'));
+      setTimeout(() => {
+        rejected(new Error('User not found'));
+      }, 500);
     }
 
-    setTimeout(
+    setTimeout(() => {
       rulfilled({
         name: 'John',
         age: 17,
         userId,
         email: `${userId}@example.com`,
-      }),
-      1000,
-    );
+      });
+    }, 1000);
   });
 
   return prRequestUserData;
 };
 
 // test-data
-// requestUserData('userid777')
-//   .then(data => console.log(data))
-//   .catch(error => console.log(error));
+requestUserData('userid777')
+  .then(user => console.log(user))
+  .catch(error => console.log(error));
 
-// requestUserData('broken')
-//   .then(data => console.log(data))
-//   .catch(error => console.log(error));
+requestUserData('broken')
+  .then(user => console.log(user))
+  .catch(error => console.log(error));
