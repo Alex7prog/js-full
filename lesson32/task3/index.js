82949ebf885cdd93093ab4b2f4b7776207f8a5cb
Alex Sedmoy <alexsedmoy@gmail.com>
2022-07-11 17:43:08 +0300

    Converter to promise

---
+++ lesson32/task3/index.js
@@ -0,0 +1,17 @@
+// Converter to promise
+
+/*
+ * ф-ция makePromise должна вернуть промис со значением переданным в ф-цию
+ */
+
+export const makePromise = number =>
+  new Promise(resolve => {
+    resolve(number);
+  });
+
+/*
+ * пример использования
+ */
+makePromise(17).then(number => {
+  console.log(number); // 17
+});
