0a0110ba1968406e062cbfaf0ed3a1902f703443
Alex Sedmoy <alexsedmoy@gmail.com>
2022-07-13 22:16:58 +0300

    Create user form

---
+++ lesson34/task3/index.js
@@ -0,0 +1,52 @@
+// Create user form
+
+const userFormElem = document.querySelector('.login-form');
+const emailUserElem = document.querySelector('[name="email"]');
+const nameUserElem = document.querySelector('[name="name"]');
+const passwordUserElem = document.querySelector('[name="password"]');
+
+const registerUserBtn = document.querySelector('.submit-button');
+
+const baseUrl = 'https://62cde2d7066bd2b6992da5e6.mockapi.io/api/v1/login_user';
+
+function sendUserData(userData) {
+  return fetch(baseUrl, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json;charset=utf-8',
+    },
+    body: JSON.stringify(userData),
+  });
+}
+
+const onValidateInput = event => {
+  if (userFormElem.reportValidity()) {
+    registerUserBtn.removeAttribute('disabled');
+    return;
+  }
+
+  registerUserBtn.setAttribute('disabled', true);
+};
+
+const clearFormData = () => {
+  emailUserElem.value = '';
+  nameUserElem.value = '';
+  passwordUserElem.value = '';
+};
+
+const onFormSubmit = event => {
+  event.preventDefault();
+
+  const formData = Object.fromEntries(new FormData(userFormElem));
+
+  sendUserData(formData).then(response => {
+    alert(`status: ${response.status}, statusText: ${response.statusText}`);
+    clearFormData();
+  });
+};
+
+emailUserElem.addEventListener('input', onValidateInput);
+nameUserElem.addEventListener('input', onValidateInput);
+passwordUserElem.addEventListener('input', onValidateInput);
+
+userFormElem.addEventListener('submit', onFormSubmit);
