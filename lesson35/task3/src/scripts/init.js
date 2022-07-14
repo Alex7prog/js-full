4c7d1f7f29d8b4ee7fb39b20eff9fb4c7df216fe
Alex Sedmoy <alexsedmoy@gmail.com>
2022-07-14 23:16:22 +0300

    List of user Github repositories

--- lesson35/task3/src/scripts/init.js
+++ lesson35/task3/src/scripts/init.js
@@ -1,6 +1,7 @@
 export const init = () => {
   const userAvatarElem = document.querySelector('.user__avatar');
-  const defaultAvatarUrl = 'https://avatars.githubusercontent.com/u/0?v=4';
+  // const defaultAvatarUrl = 'https://avatars.githubusercontent.com/u/0?v=4';
+  const defaultAvatarUrl = 'https://avatars3.githubusercontent.com/u10001';
 
   userAvatarElem.src = defaultAvatarUrl;
 };
