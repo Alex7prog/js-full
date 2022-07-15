f2659a79e4c0a72a9116e01844ee22da81dee310
Alex Sedmoy <alexsedmoy@gmail.com>
2022-07-15 12:53:09 +0300

    List of user Github repositories

--- lesson35/task3/src/scripts/defaultUser.js
+++ lesson35/task3/src/scripts/defaultUser.js
@@ -1,8 +1,8 @@
 import { renderUserData } from './renderUser.js';
 
 export const initDefaultUser = () => {
-  const defaultAvatarUrl = 'https://avatars.githubusercontent.com/u/0?v=4';
-  // const defaultAvatarUrl = 'https://avatars3.githubusercontent.com/u10001';
+  // const defaultAvatarUrl = 'https://avatars.githubusercontent.com/u/0?v=4';
+  const defaultAvatarUrl = 'https://avatars3.githubusercontent.com/u10001';
 
   const defaultUser = {
     avatar_url: defaultAvatarUrl,
