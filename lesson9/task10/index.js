2b1c6408e87f46a660da403b74e6bc0942b16eaf
Alex Sedmoy <alexsedmoy@gmail.com>
2022-07-16 17:45:28 +0300

    compare objects

--- lesson9/task10/index.js
+++ lesson9/task10/index.js
@@ -1,23 +1,27 @@
 // compare objects
 
-function compareObjects(obj1, obj2) {
-  const sortObj1 = Object.keys(obj1)
-    .sort()
-    .reduce((result, key) => {
-      result[key] = obj1[key];
-      return result;
-    }, {});
-  const sortObj2 = Object.keys(obj2)
-    .sort()
-    .reduce((result, key) => {
-      result[key] = obj2[key];
-      return result;
-    }, {});
+// Algo:
+// compare [obj1.keys] !== [obj2.keys]
+// iteration [obj1.keys] =>  exist obj2[keys] & equal obj1, obj2 value by key
+// choose method some (some iteration < every iteration)
 
-  return JSON.stringify(sortObj1) === JSON.stringify(sortObj2);
-}
+// function compareObjects(obj1, obj2) {
+//   const keysObj1 = Object.keys(obj1);
 
-// examples
+//   if (keysObj1.length !== Object.keys(obj2).length) {
+//     return false;
+//   }
+
+//   return !keysObj1.some(key => !hasOwnProperty.call(obj2, key) || obj1[key] !== obj2[key]);
+// }
+
+// refactoring
+const compareObjects = (obj1, obj2) =>
+  Object.keys(obj1).length !== Object.keys(obj2).length
+    ? false
+    : !Object.keys(obj1).some(key => !hasOwnProperty.call(obj2, key) || obj1[key] !== obj2[key]);
+
+// test-data
 const obj1 = {
   name: 'Tom',
   age: 17,
@@ -39,9 +43,32 @@ const obj4 = {
   age: 17,
 };
 
-compareObjects(obj1, obj2); // ==> false
+const obj5 = {
+  student: false,
+  name: 'Bob',
+  age: 17,
+};
+
 console.log('compareObjects(obj1, obj2): ', compareObjects(obj1, obj2));
-compareObjects(obj2, obj3); // ==> false
 console.log('compareObjects(obj2, obj3): ', compareObjects(obj2, obj3));
-compareObjects(obj1, obj4); // ==> true
 console.log('compareObjects(obj1, obj4): ', compareObjects(obj1, obj4));
+console.log('compareObjects(obj3, obj2): ', compareObjects(obj3, obj2));
+console.log('compareObjects(obj3, obj5): ', compareObjects(obj3, obj5));
+
+// option 2 - JSON.stringify
+// function compareObjects(obj1, obj2) {
+//   const sortObj1 = Object.keys(obj1)
+//     .sort()
+//     .reduce((result, key) => {
+//       result[key] = obj1[key];
+//       return result;
+//     }, {});
+//   const sortObj2 = Object.keys(obj2)
+//     .sort()
+//     .reduce((result, key) => {
+//       result[key] = obj2[key];
+//       return result;
+//     }, {});
+
+//   return JSON.stringify(sortObj1) === JSON.stringify(sortObj2);
+// }
