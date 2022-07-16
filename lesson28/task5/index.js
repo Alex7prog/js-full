ead52146109215839b4878593806bac8729abd36
Alex Sedmoy <alexsedmoy@gmail.com>
2022-07-16 19:38:39 +0300

    shmoment

--- lesson28/task5/index.js
+++ lesson28/task5/index.js
@@ -2,37 +2,88 @@
 
 // in: Date / out: newDate{methods}
 
-export const shmoment = dateValue => {
-  const changeDateTime = {
-    years: (valueDate, timeInterval) => {
-      return valueDate.setFullYear(valueDate.getFullYear() + timeInterval);
-    },
-    months: (valueDate, timeInterval) => {
-      return valueDate.setMonth(valueDate.getMonth() + timeInterval);
-    },
-    days: (valueDate, timeInterval) => {
-      return valueDate.setDate(valueDate.getDate() + timeInterval);
-    },
-    hours: (valueDate, timeInterval) => {
-      return valueDate.setHours(valueDate.getHours() + timeInterval);
-    },
-    minutes: (valueDate, timeInterval) => {
-      return valueDate.setMinutes(valueDate.getMinutes() + timeInterval);
-    },
-    seconds: (valueDate, timeInterval) => {
-      return valueDate.setSeconds(valueDate.getSeconds() + timeInterval);
-    },
-    milliseconds: (valueDate, timeInterval) => {
-      return valueDate.setMilliseconds(valueDate.getMilliseconds() + timeInterval);
-    },
-  };
+// My decision
+// export const shmoment = dateValue => {
+//   const changeDateTime = {
+//     years: (valueDate, timeInterval) => {
+//       return valueDate.setFullYear(valueDate.getFullYear() + timeInterval);
+//     },
+//     months: (valueDate, timeInterval) => {
+//       return valueDate.setMonth(valueDate.getMonth() + timeInterval);
+//     },
+//     days: (valueDate, timeInterval) => {
+//       return valueDate.setDate(valueDate.getDate() + timeInterval);
+//     },
+//     hours: (valueDate, timeInterval) => {
+//       return valueDate.setHours(valueDate.getHours() + timeInterval);
+//     },
+//     minutes: (valueDate, timeInterval) => {
+//       return valueDate.setMinutes(valueDate.getMinutes() + timeInterval);
+//     },
+//     seconds: (valueDate, timeInterval) => {
+//       return valueDate.setSeconds(valueDate.getSeconds() + timeInterval);
+//     },
+//     milliseconds: (valueDate, timeInterval) => {
+//       return valueDate.setMilliseconds(valueDate.getMilliseconds() + timeInterval);
+//     },
+//   };
 
+//   let resultDate = new Date(dateValue);
+
+//   const moment = {
+//     days: () => {},
+//     add(timeUnit, dateValue) {
+//       resultDate = changeDateTime[timeUnit](new Date(resultDate), dateValue);
+//       return this;
+//     },
+//     subtract(timeUnit, dateValue) {
+//       this.add(timeUnit, -dateValue);
+//       return this;
+//     },
+
+//     result() {
+//       return new Date(resultDate);
+//     },
+//   };
+
+//   return moment;
+// };
+
+// Refactoring
+
+const getMethodsName = {
+  years: 'getFullYear',
+  months: 'getMonth',
+  days: 'getDate',
+  hours: 'getHours',
+  minutes: 'getMinutes',
+  seconds: 'getSeconds',
+  milliseconds: 'getMilliseconds',
+};
+
+const setMethodsName = {
+  years: 'setFullYear',
+  months: 'setMonth',
+  days: 'setDate',
+  hours: 'setHours',
+  minutes: 'setMinutes',
+  seconds: 'setSeconds',
+  milliseconds: 'setMilliseconds',
+};
+
+export const shmoment = dateValue => {
   let resultDate = new Date(dateValue);
 
   const moment = {
     days: () => {},
     add(timeUnit, dateValue) {
-      resultDate = changeDateTime[timeUnit](new Date(resultDate), dateValue);
+      const currentUnitDateValue = resultDate[getMethodsName[timeUnit]]();
+      resultDate = new Date(resultDate[setMethodsName[timeUnit]](currentUnitDateValue + dateValue));
+      // resultDate = new Date(
+      //   resultDate[setMethodsName[timeUnit]](
+      //     new Date(resultDate)[getMethodsName[timeUnit]]() + dateValue,
+      //   ),
+      // );
       return this;
     },
     subtract(timeUnit, dateValue) {
