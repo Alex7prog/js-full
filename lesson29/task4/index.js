b9d56f670ed37aa38a00ea8d135ec35415537820
Alex Sedmoy <alexsedmoy@gmail.com>
2022-07-18 20:45:05 +0300

    Function for call delay

--- lesson29/task4/index.js
+++ lesson29/task4/index.js
@@ -1,9 +1,15 @@
 // Function for call delay
 
+// option 1
 export const delay = (delay, callback, context, ...arg) => {
-  setTimeout(callback.bind(context, ...arg), delay);
+  setTimeout(() => callback.call(context, arg), delay);
 };
+
+// option 2
+// export const delay = (delay, callback, context, ...arg) => {
+//   setTimeout(callback.bind(context, arg), delay);
+// };
 
 const func = (...arg) => console.log(`callback function arguments: ${arg}`);
 
-delay(3000, func, delay, 1, 3, 4, 5);
+delay(3000, func, delay, 1, 3, 4, 5, 7);
